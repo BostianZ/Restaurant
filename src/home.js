ed8d5638a4d2308d2b3d18d00140577599f798c5
BostianZ <zzaacckk@gmail.com>
2023-11-24 12:20:12 -0500

    Added more styling to menu items, working on figuring out a better styling idea

--- src/home.js
+++ src/home.js
@@ -1,20 +1,37 @@
+class HomeContent {
+    constructor(heading, description) {
+        this.heading = heading;
+        this.description = description;
+    }
+
+    renderItem() {
+        const content = document.querySelector("#content");
+        const divEl = document.createElement("div");
+        const headingEl = document.createElement("h2");
+        const paraEl = document.createElement("p");
+
+        headingEl.textContent = this.heading;
+        paraEl.textContent = this.description;
+
+        divEl.appendChild(headingEl);
+        divEl.appendChild(paraEl);
+
+        content.appendChild(divEl);
+    }
+}
+
 function home() {
     const content = document.querySelector("#content");
-    const divEl = document.createElement("div");
-    const headingEl = document.createElement("h2");
-    const paraEl = document.createElement("p")
-
     content.innerHTML = ""
 
-    headingEl.textContent = "Heading here"
-    paraEl.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-   
-    divEl.appendChild(headingEl);
-    divEl.appendChild(paraEl);
-    content.appendChild(divEl);
+    new HomeContent("Restaurant Name", `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`).renderItem();
+
+    new HomeContent("Hours", "We are open M-F from 11am to 11pm. Closed on Saturdays and Sundays.").renderItem();
+
+    new HomeContent("Location", "12345 Invisible street, Fake place, Real state").renderItem();
 }
 
 export { home }
--- src/menu.js
+++ src/menu.js
@@ -1,29 +1,48 @@
 class MenuItem {
-    constructor(title, description) {
+    constructor(title, description, image) {
       this.title = title;
       this.description = description;
+      this.image = image;
     }
     
     renderItem() {
       const contentEl = document.querySelector("#content");
       const titleEl = document.createElement("h2");
       const descriptEl = document.createElement("p");
+      const divEl = document.createElement("div");
+      const img = document.createElement("img");
+
+      img.setAttribute("src", this.image);
+      divEl.classList.add("menu-item");
+
       titleEl.textContent = this.title;
       descriptEl.textContent = this.description;
       
-      contentEl.appendChild(titleEl);
-      contentEl.appendChild(descriptEl);
+      divEl.appendChild(titleEl);
+      divEl.appendChild(descriptEl);
+      divEl.appendChild(img);
+      contentEl.appendChild(divEl);
     }
 
   }
+
+function menuHeading() {
+    const content = document.querySelector("#content")
+    const divEl = document.createElement("h1");
+    divEl.textContent = "Menu";
+
+    content.appendChild(divEl);
+}
   
 function renderMenuItems() {
     const contentEl = document.querySelector("#content");
     contentEl.innerHTML = "";
+    menuHeading();
 
-    new MenuItem("Hello", "World").renderItem();
-    new MenuItem("Hello", "World").renderItem();
-    new MenuItem("Hello", "World").renderItem();
+    new MenuItem("Hello", "World", "../dist/image01.jpg").renderItem();
+    new MenuItem("Hello", "World", "../dist/image01.jpg").renderItem();
+    new MenuItem("Hello", "World", "../dist/image01.jpg").renderItem();
+    
 }
 
 export { renderMenuItems };
